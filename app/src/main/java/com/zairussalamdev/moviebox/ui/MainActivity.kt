86922a348d57a2d65dc898e1246ffb92cab7d0b7
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-04-16 08:44:39 +0700

    Add SplashScreen

--- app/src/main/java/com/zairussalamdev/moviebox/ui/MainActivity.kt
+++ app/src/main/java/com/zairussalamdev/moviebox/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.zairussalamdev.moviebox
+package com.zairussalamdev.moviebox.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zairussalamdev.moviebox.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/zairussalamdev/moviebox/ui/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.zairussalamdev.moviebox.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.zairussalamdev.moviebox.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    companion object {
+        const val SPLASH_SCREEN_DELAY_MILLS = 2500L
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_SCREEN_DELAY_MILLS)
+    }
+}
