9c85d8d670a791ed0b5c32909adcbcd0011483f8
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-04-20 22:01:30 +0700

    Add Repository Tests

--- app/src/test/java/com/zairussalamdev/moviebox/data/TMDBRepositoryTest.kt
+++ app/src/test/java/com/zairussalamdev/moviebox/data/TMDBRepositoryTest.kt
@@ -1,10 +1,87 @@
 package com.zairussalamdev.moviebox.data
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.verify
+import com.zairussalamdev.moviebox.data.remote.RemoteDataSource
+import com.zairussalamdev.moviebox.utils.DummyData
+import com.zairussalamdev.moviebox.utils.TestCoroutineRule
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
+@ExperimentalCoroutinesApi
 class TMDBRepositoryTest {
-    @Rule
+    @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    @Mock
+    private lateinit var remoteDataSource: RemoteDataSource
+
+    private lateinit var tmdbRepository: TMDBRepository
+
+    @Before
+    fun init() {
+        tmdbRepository = TMDBRepository(remoteDataSource)
+    }
+
+    @Test
+    fun `get movie list success`() {
+        testCoroutineRule.runBlockingTest {
+            val response = DummyData.getDummyMovieListResponse()
+            `when`(remoteDataSource.getMovieList()).thenReturn(response)
+            val result = tmdbRepository.getMovieList()
+            assertNotNull(result)
+            verify(remoteDataSource).getMovieList()
+            assertEquals(response.movies.size, result.size)
+        }
+    }
+
+    @Test
+    fun `get tv show list success`() {
+        testCoroutineRule.runBlockingTest {
+            val response = DummyData.getDummyTvShowListResponse()
+            `when`(remoteDataSource.getTvShowList()).thenReturn(response)
+            val result = tmdbRepository.getTvShowsList()
+            assertNotNull(result)
+            verify(remoteDataSource).getTvShowList()
+            assertEquals(response.tvShows.size, result.size)
+        }
+    }
+
+    @Test
+    fun `get movie detail success`() {
+        testCoroutineRule.runBlockingTest {
+            val id = 1
+            val response = DummyData.getDummyMovieDetailResponse()
+            `when`(remoteDataSource.getMovieDetail(id)).thenReturn(response)
+            val result = tmdbRepository.getMovieDetail(id)
+            assertNotNull(result)
+            verify(remoteDataSource).getMovieDetail(id)
+            assertEquals(response.title, result.title)
+        }
+    }
+
+    @Test
+    fun `get tv show detail success`() {
+        testCoroutineRule.runBlockingTest {
+            val id = 1
+            val response = DummyData.getDummyTvShowDetailResponse()
+            `when`(remoteDataSource.getTvShowDetail(id)).thenReturn(response)
+            val result = tmdbRepository.getTvShowDetail(id)
+            assertNotNull(result)
+            verify(remoteDataSource).getTvShowDetail(id)
+            assertEquals(response.title, result.title)
+        }
+    }
 }
--- app/src/test/java/com/zairussalamdev/moviebox/utils/DummyData.kt
+++ app/src/test/java/com/zairussalamdev/moviebox/utils/DummyData.kt
@@ -2,6 +2,7 @@ package com.zairussalamdev.moviebox.utils
 
 import com.zairussalamdev.moviebox.data.entities.DetailEntity
 import com.zairussalamdev.moviebox.data.entities.MovieEntity
+import com.zairussalamdev.moviebox.data.remote.responses.*
 
 object DummyData {
     fun getDummyListData(): List<MovieEntity> {
@@ -29,14 +30,66 @@ object DummyData {
 
     fun getDummyDetailData(): DetailEntity {
         return DetailEntity(
-            id = 1,
-            title = "title",
-            overview = "overview",
-            posterPath = "posterPath",
-            voteAverage = 5.0,
-            tagLine = "tagLine",
-            homepage = "homepage",
-            status = "status"
+                id = 1,
+                title = "title",
+                overview = "overview",
+                posterPath = "posterPath",
+                voteAverage = 5.0,
+                tagLine = "tagLine",
+                homepage = "homepage",
+                status = "status"
+        )
+    }
+
+    fun getDummyMovieListResponse(): MovieResponse {
+        return MovieResponse(
+                movies = mutableListOf(
+                        Movie("overview1", "title1", "poster1", 5.6, 1),
+                        Movie("overview2", "title2", "poster2", 5.6, 2),
+                        Movie("overview3", "title3", "poster3", 5.6, 3),
+                )
+        )
+
+    }
+
+    fun getDummyMovieDetailResponse(): DetailResponse {
+        return DetailResponse(
+                "overview",
+                "title",
+                "poster",
+                9.9,
+                "tagline",
+                listOf(Genre("genre1")),
+                1,
+                "homepage",
+                "release",
+                12.0,
+        )
+    }
+
+    fun getDummyTvShowListResponse(): TvShowResponse {
+        return TvShowResponse(
+                tvShows = mutableListOf(
+                        TvShow("overview1", "poster1", 5.0, "name1", 1),
+                        TvShow("overview2", "poster2", 4.0, "name2", 2),
+                        TvShow("overview3", "poster3", 5.0, "name3", 3),
+                )
+        )
+
+    }
+
+    fun getDummyTvShowDetailResponse(): DetailResponse {
+        return DetailResponse(
+                "overview",
+                "title",
+                "poster",
+                9.9,
+                "tagline",
+                listOf(Genre("genre1")),
+                1,
+                "homepage",
+                "release",
+                12.0,
         )
     }
 }
