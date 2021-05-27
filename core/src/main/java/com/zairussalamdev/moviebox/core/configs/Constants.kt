a6ba9279009f7bc1aea36ab5128f1f1c2e518255
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-05-27 11:37:07 +0700

    Edit DB Passphrase

--- core/src/main/java/com/zairussalamdev/moviebox/core/configs/Constants.kt
+++ core/src/main/java/com/zairussalamdev/moviebox/core/configs/Constants.kt
@@ -3,6 +3,7 @@ package com.zairussalamdev.moviebox.core.configs
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val BASE_URL_SHA256_KEY = "+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0="
+    const val DB_PASSPHRASE = "MOVIEBOX_AWESOME_DATABASE"
     const val THUMBNAIL_IMAGE_URL = "https://image.tmdb.org/t/p/w300/"
     const val FULL_SIZE_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
     const val DB_NAME = "MovieBoxDB"
