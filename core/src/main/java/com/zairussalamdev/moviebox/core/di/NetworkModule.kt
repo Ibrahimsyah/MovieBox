3c42ab53e5541f8a16dce49d84a2854daeafa2d0
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-05-27 10:19:10 +0700

    Add Certificate Pinner

--- core/src/main/java/com/zairussalamdev/moviebox/core/di/NetworkModule.kt
+++ core/src/main/java/com/zairussalamdev/moviebox/core/di/NetworkModule.kt
@@ -2,11 +2,27 @@ package com.zairussalamdev.moviebox.core.di
 
 import com.zairussalamdev.moviebox.core.configs.Constants
 import com.zairussalamdev.moviebox.core.data.source.remote.network.TMDBService
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val networkModule = module {
+    single {
+        val hostName = Constants.BASE_URL
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostName, Constants.BASE_URL_SHA256_KEY)
+            .build()
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
+    }
     single {
         Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
