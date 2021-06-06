a31b592cc764e0870fcd8009e98c653b4a5e71fc
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-06-06 23:28:20 +0700

    Remove unnecessary code

--- core/src/main/java/com/zairussalamdev/moviebox/core/data/source/local/LocalDataSource.kt
+++ core/src/main/java/com/zairussalamdev/moviebox/core/data/source/local/LocalDataSource.kt
@@ -8,40 +8,27 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
 class LocalDataSource constructor(private val movieDao: MovieDao) {
+
+    fun getMovies(): Flow<List<MovieEntity>> = movieDao.getMovies()
+
+    fun getMovieDetail(movieId: Int): Flow<DetailEntity?> = movieDao.getMovieDetail(movieId)
+
+    fun getTvShows(): Flow<List<MovieEntity>> = movieDao.getTvShows()
+
+    fun getTvShowDetail(tvShowId: Int): Flow<DetailEntity?> = movieDao.getTvShowDetail(tvShowId)
+
+    fun getFavoriteMovies(): Flow<List<MovieEntity>> = movieDao.getFavoriteMovies()
+
+    fun getFavoriteTvShows(): Flow<List<MovieEntity>> = movieDao.getFavoriteTvShows()
+
+    fun checkMovieFavorite(id: Int): Flow<Boolean> = movieDao.checkMovieFavorite(id)
+
     suspend fun insertMovie(movies: List<MovieEntity>) {
         withContext(Dispatchers.IO) { movieDao.insertMovie(movies) }
     }
 
     suspend fun insertDetailMovie(movieDetail: DetailEntity) {
         withContext(Dispatchers.IO) { movieDao.insertDetail(movieDetail) }
-    }
-
-    fun getMovies(): Flow<List<MovieEntity>> {
-        return movieDao.getMovies()
-    }
-
-    fun getMovieDetail(movieId: Int): Flow<DetailEntity?> {
-        return movieDao.getMovieDetail(movieId)
-    }
-
-    fun getTvShows(): Flow<List<MovieEntity>> {
-        return movieDao.getTvShows()
-    }
-
-    fun getTvShowDetail(tvShowId: Int): Flow<DetailEntity?> {
-        return movieDao.getTvShowDetail(tvShowId)
-    }
-
-    fun getFavoriteMovies(): Flow<List<MovieEntity>> {
-        return movieDao.getFavoriteMovies()
-    }
-
-    fun getFavoriteTvShows(): Flow<List<MovieEntity>> {
-        return movieDao.getFavoriteTvShows()
-    }
-
-    fun checkMovieFavorite(id: Int): Flow<Boolean> {
-        return movieDao.checkMovieFavorite(id)
     }
 
     suspend fun addFavoriteMovie(id: Int) {
